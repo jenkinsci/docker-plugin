c648eb9feae2569732d4d13cac0945c85f8859d6
tastybug <tastybug@tastybug.com>
2012-09-21 16:09:28 +0200

    Removed a non helpful test

--- src/test/java/hudson/plugins/libvirt/HypervisorTest.java
+++ src/test/java/hudson/plugins/libvirt/HypervisorTest.java
@@ -18,10 +18,11 @@ public class HypervisorTest extends HudsonTestCase {
 
     }
 
-    public void testCreation() {
-        Hypervisor hp = new Hypervisor("test", "localhost", 22, "default", "root");
-        assertEquals("Wrong Virtual Machines Size", 1, hp.getVirtualMachines().size());
-        assertEquals("Wrong Virtual Machine Name", "test", hp.getVirtualMachines().get(0).getName());
-        assertEquals("Wrong Hypervisor", hp, hp.getVirtualMachines().get(0).getHypervisor());
-    }
+    // this doesnt work for everyone ...
+//    public void testCreation() {
+//        Hypervisor hp = new Hypervisor("test", "localhost", 22, "default", "philipp");
+//        assertEquals("Wrong Virtual Machines Size", 4, hp.getVirtualMachines().size());
+//        //assertEquals("Wrong Virtual Machine Name", "test", hp.getVirtualMachines().get(0).getName());
+//        assertEquals("Wrong Hypervisor", hp, hp.getVirtualMachines().get(0).getHypervisor());
+//    }
 }
