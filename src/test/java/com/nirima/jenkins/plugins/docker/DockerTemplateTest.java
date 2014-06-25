c55fccab24c75550bbdcc003dd9e775bf95f55bc
Mark Gius <mgius7096@gmail.com>
2014-06-25 12:46:09 -0700

    Proper parsing of empty dnsHosts string
    When DNS hosts are empty, plugin sends DNS override to docker container
    ("Dns":[""]).  With this patch, correctly declines to override dns when
    the cloud configuration does not ask for it.

---
+++ src/test/java/com/nirima/jenkins/plugins/docker/DockerTemplateTest.java
@@ -0,0 +1,35 @@
+package com.nirima.jenkins.plugins.docker;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class DockerTemplateTest {
+
+    private DockerTemplate getDockerTemplateInstanceWithDNSHost(String dnsString) {
+        DockerTemplate instance = new DockerTemplate("image", null, "remoteFs", "credentialsId", " jvmOptions", " javaPath", "prefixStartSlaveCmd", " suffixStartSlaveCmd", "", dnsString, "dockerCommand", "volumes", "hostname", false);
+        return instance;
+    }
+
+    @Test
+    public void testDnsHosts() {
+        DockerTemplate instance;
+        String[] expected;
+
+        instance = getDockerTemplateInstanceWithDNSHost("");
+        assertEquals(0, instance.dnsHosts.length);
+
+        instance = getDockerTemplateInstanceWithDNSHost("8.8.8.8");
+        expected = new String[]{"8.8.8.8"};
+
+        assertEquals(1, instance.dnsHosts.length);
+        assertArrayEquals(expected, instance.dnsHosts);
+
+        instance = getDockerTemplateInstanceWithDNSHost("8.8.8.8 8.8.4.4");
+        expected = new String[]{"8.8.8.8", "8.8.4.4"};
+
+        assertEquals(2, instance.dnsHosts.length);
+        assertArrayEquals(expected, instance.dnsHosts);
+
+    }
+
+}
