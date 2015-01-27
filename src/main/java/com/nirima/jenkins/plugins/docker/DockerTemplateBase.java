ed66d21b548bae1308a5b01c19f4165c5d96326c
Carlos Sanchez <carlos@apache.org>
2015-01-27 12:24:42 +0100

    Make methods public so they can be reused

--- src/main/java/com/nirima/jenkins/plugins/docker/DockerTemplateBase.java
+++ src/main/java/com/nirima/jenkins/plugins/docker/DockerTemplateBase.java
@@ -5,9 +5,7 @@ import com.google.common.base.Joiner;
 import com.google.common.base.Objects;
 import com.google.common.base.Splitter;
 import com.google.common.base.Strings;
-import com.google.common.collect.Collections2;
 import com.google.common.collect.Iterables;
-
 import com.github.dockerjava.api.DockerClient;
 import com.github.dockerjava.api.DockerException;
 import com.github.dockerjava.api.command.CreateContainerCmd;
@@ -136,7 +134,7 @@ public abstract class DockerTemplateBase {
 
     }
 
-    protected String[] getDockerCommandArray() {
+    public String[] getDockerCommandArray() {
          String[] dockerCommandArray = new String[0];
 
         if(dockerCommand != null && !dockerCommand.isEmpty()){
@@ -145,7 +143,7 @@ public abstract class DockerTemplateBase {
         return dockerCommandArray;
     }
 
-    protected Iterable<PortBinding> getPortMappings() {
+    public Iterable<PortBinding> getPortMappings() {
 
         if(Strings.isNullOrEmpty(bindPorts) ) {
             return Collections.EMPTY_LIST;
@@ -224,7 +222,7 @@ public abstract class DockerTemplateBase {
     }
 
 
-    protected List<LxcConf> getLxcConf() {
+    public List<LxcConf> getLxcConf() {
         List<LxcConf> temp = new ArrayList<LxcConf>();
         if( lxcConfString == null || lxcConfString.trim().equals(""))
             return temp;
