06b32d794b94ef98b3cf93c356242ee58634ddd7
Kanstantsin Shautsou <Kanstantsin_Shautsou@epam.com>
2015-05-22 19:36:25 +0300

    Test cases for volume and volumeFrom

---
+++ docker-plugin/src/test/java/com/nirima/jenkins/plugins/docker/DockerTemplateTest2.java
@@ -0,0 +1,33 @@
+package com.nirima.jenkins.plugins.docker;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.jvnet.hudson.test.JenkinsRule;
+import org.jvnet.hudson.test.recipes.LocalData;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author Kanstantsin Shautsou
+ */
+public class DockerTemplateTest2 {
+    @Rule
+    public JenkinsRule j = new JenkinsRule();
+
+    @Test
+    @LocalData
+    public void testLoad090rc1Config() {
+        final DockerCloud dock = (DockerCloud) j.getInstance().getCloud("dock");
+        final DockerTemplate template = dock.getTemplate("image:b25");
+        final String[] volumes = template.getVolumes();
+
+        assertEquals(volumes.length, 2);
+        assertEquals(volumes[0], "/host/path:/container/path:ro");
+        assertEquals(volumes[1], "/host/path2:/container/path2:ro");
+
+        final String[] volumesFrom2 = template.getVolumesFrom2();
+        assertEquals(volumesFrom2.length, 1);
+        assertEquals(volumesFrom2[0], "otherContainer:ro");
+        assertNull(template.getVolumesFrom());
+    }
+}
