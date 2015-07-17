3a9f1c378b9ab9f975d15dc874fa601d5653bb45
Kanstantsin Shautsou <kanstantsin.sha@gmail.com>
2015-07-17 19:50:33 +0300

    Fix #263. Load 0.8.0 config data.

--- docker-plugin/src/test/java/com/nirima/jenkins/plugins/docker/DockerTemplate2Test.java
+++ docker-plugin/src/test/java/com/nirima/jenkins/plugins/docker/DockerTemplate2Test.java
@@ -1,15 +1,13 @@
 package com.nirima.jenkins.plugins.docker;
 
-import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
+import org.jvnet.hudson.test.Issue;
 import org.jvnet.hudson.test.JenkinsRule;
 import org.jvnet.hudson.test.recipes.LocalData;
 
 import static java.util.Arrays.asList;
-import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.hasSize;
-import static org.hamcrest.Matchers.nullValue;
+import static org.hamcrest.Matchers.*;
 import static org.junit.Assert.*;
 
 /**
@@ -18,6 +16,33 @@ import static org.junit.Assert.*;
 public class DockerTemplate2Test {
     @Rule
     public JenkinsRule j = new JenkinsRule();
+
+    @Test
+    @LocalData
+    @Issue("jenkinsci/docker-plugin/issues/263")
+    public void load080Config() {
+        final DockerCloud dock = (DockerCloud) j.getInstance().getCloud("someCloud");
+        assertThat(dock.getTemplates(), hasSize(6));
+
+        final DockerTemplate template = dock.getTemplate("somedomain.com:5000/dockerbuild");
+        assertThat(template, notNullValue());
+
+        assertThat(template.getLabelString(), equalTo("dockerbuild-local"));
+        assertThat(template.remoteFs, equalTo("/home/jenkins"));
+
+        final DockerTemplateBase tBase = template.getDockerTemplateBase();
+        assertThat(tBase, notNullValue());
+
+        assertThat(tBase.getDockerCommandArray()[0], equalTo("/usr/bin/start-jenkins-slave.sh"));
+        assertThat(tBase.getDnsString(), equalTo("8.8.8.8"));
+
+        assertThat(tBase.getVolumesString(), equalTo("/dev/log:/dev/log"));
+        assertThat(asList(tBase.getVolumes()), hasSize(1));
+        assertThat(tBase.getVolumes()[0], equalTo("/dev/log:/dev/log"));
+
+        assertFalse(tBase.bindAllPorts);
+        assertTrue(tBase.privileged);
+    }
 
     @Test
     @LocalData
