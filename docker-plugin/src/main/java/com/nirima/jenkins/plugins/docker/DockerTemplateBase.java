cfb346bfad620c7abc1385b5669c164d3d75b81c
Nigel Magnay <nigel.magnay@gmail.com>
2016-08-14 11:44:16 +0100

    Add imports.

--- docker-plugin/src/main/java/com/nirima/jenkins/plugins/docker/DockerTemplateBase.java
+++ docker-plugin/src/main/java/com/nirima/jenkins/plugins/docker/DockerTemplateBase.java
@@ -38,7 +38,9 @@ import javax.annotation.CheckForNull;
 import javax.annotation.Nullable;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.logging.Logger;
 
 import static org.apache.commons.lang.StringUtils.trimToNull;
