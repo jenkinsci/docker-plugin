41ae3dcd48ee4cab8c1f2de25486fa258ace2677
Nicolas De Loof <nicolas.deloof@gmail.com>
2017-09-11 10:06:55 +0200

    stop using a shaded docker-java depend on an API plugin to provide this dependency

--- docker-plugin/src/main/java/com/nirima/jenkins/plugins/docker/client/ClientBuilderForPlugin.java
+++ docker-plugin/src/main/java/com/nirima/jenkins/plugins/docker/client/ClientBuilderForPlugin.java
@@ -4,12 +4,8 @@ import com.github.dockerjava.api.DockerClient;
 import com.github.dockerjava.api.command.DockerCmdExecFactory;
 import com.github.dockerjava.core.DockerClientBuilder;
 import com.github.dockerjava.core.DockerClientConfig;
-import com.github.dockerjava.core.DockerClientImpl;
-import com.github.dockerjava.jaxrs.DockerCmdExecFactoryImpl;
+import com.github.dockerjava.netty.NettyDockerCmdExecFactory;
 import org.apache.commons.lang.Validate;
-
-import java.io.Serializable;
-import java.util.ServiceLoader;
 
 /**
  * Builds ClientConfig with helper methods that extracts info for plugin routines.
@@ -35,11 +31,11 @@ public class ClientBuilderForPlugin {
     }
 
     public ClientBuilderForPlugin withDockerCmdExecConfig(DockerCmdExecConfig config) {
-        DockerCmdExecFactoryImpl impl = new DockerCmdExecFactoryImpl();
+        NettyDockerCmdExecFactory impl = new NettyDockerCmdExecFactory();
         this.dockerCmdExecFactory = impl;
 
-        if( config.getReadTimeoutMillis() != null )
-            impl.withReadTimeout(config.getReadTimeoutMillis());
+        //if( config.getReadTimeoutMillis() != null )
+        //    impl.withReadTimeout(config.getReadTimeoutMillis());
 
         if( config.getConnectTimeoutMillis() != null )
             impl.withConnectTimeout(config.getConnectTimeoutMillis());
