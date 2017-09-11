ad076e91551484fcc0d60c3256d4519ad9e8bba1
Nicolas De Loof <nicolas.deloof@gmail.com>
2017-09-11 14:14:42 +0200

    fix #478

--- docker-plugin/src/main/resources/com/nirima/jenkins/plugins/docker/strategy/DockerCloudRetentionStrategy/config.groovy
+++ docker-plugin/src/main/resources/com/nirima/jenkins/plugins/docker/strategy/DockerCloudRetentionStrategy/config.groovy
@@ -2,6 +2,6 @@ package com.nirima.jenkins.plugins.docker.strategy.DockerCloudRetentionStrategy
 
 def f = namespace(lib.FormTagLib);
 
-f.entry(title: "Idle timeout", field: "idleMinutes") {
+f.entry(title: "Idle timeout", field: "timeout") {
     f.number(default: 0)
 }
