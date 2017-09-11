9bedfb8d8116d49922a57d536ff7b7b9bd6ec007
Nicolas De Loof <nicolas.deloof@gmail.com>
2017-09-11 12:59:25 +0200

    open stdin for interactive stdin/out channel

--- docker-plugin/src/main/java/io/jenkins/docker/AttachedDockerSlaveProvisioner.java
+++ docker-plugin/src/main/java/io/jenkins/docker/AttachedDockerSlaveProvisioner.java
@@ -4,7 +4,9 @@ import com.github.dockerjava.api.DockerClient;
 import com.github.dockerjava.api.command.CreateContainerCmd;
 import com.github.dockerjava.api.exception.DockerException;
 import com.github.dockerjava.api.model.Frame;
+import com.github.dockerjava.api.model.LogConfig;
 import com.github.dockerjava.core.command.AttachContainerResultCallback;
+import com.google.common.annotations.Beta;
 import com.nirima.jenkins.plugins.docker.DockerCloud;
 import com.nirima.jenkins.plugins.docker.DockerSlave;
 import com.nirima.jenkins.plugins.docker.DockerTemplate;
@@ -18,6 +20,8 @@ import jenkins.model.Jenkins;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.io.PipedInputStream;
 import java.io.PipedOutputStream;
 
@@ -25,12 +29,15 @@ import java.io.PipedOutputStream;
  * Create a container in interactive mode and use attached stdin/stdout as transport for remoting.
  * @author <a href="mailto:nicolas.deloof@gmail.com">Nicolas De Loof</a>
  */
+@Beta
 public class AttachedDockerSlaveProvisioner extends DockerSlaveProvisioner {
 
 
     private DockerSlave slave;
 
     private File remoting;
+    private InputStream in;
+    private OutputStream out;
 
     public AttachedDockerSlaveProvisioner(DockerCloud cloud, DockerTemplate template, DockerClient client) {
         super(cloud, template, client);
@@ -43,6 +50,14 @@ public class AttachedDockerSlaveProvisioner extends DockerSlaveProvisioner {
         this.slave = new DockerSlave(cloud, template, new NoOpLauncher());
         Jenkins.getInstance().addNode(slave);
         runContainer();
+
+        slave.getComputer().setChannel(in, out, TaskListener.NULL, new Channel.Listener() {
+            @Override
+            public void onClosed(Channel channel, IOException cause) {
+                System.out.println("Closed");
+            }
+        });
+
         return slave;
     }
 
@@ -50,6 +65,9 @@ public class AttachedDockerSlaveProvisioner extends DockerSlaveProvisioner {
     protected void prepareCreateContainerCommand(CreateContainerCmd cmd) throws DockerException, IOException {
         cmd.withAttachStdin(true)
             .withAttachStdout(true)
+            .withStdinOpen(true)
+            .withStdInOnce(true)
+            .withLogConfig(new LogConfig(LogConfig.LoggingType.NONE))
             .withCmd("java", "-jar", template.remoteFs + '/' + remoting.getName());
     }
 
@@ -63,35 +81,31 @@ public class AttachedDockerSlaveProvisioner extends DockerSlaveProvisioner {
                 .exec();
 
         final PipedInputStream containerStdin = new PipedInputStream();
+        out = new PipedOutputStream(containerStdin);
 
         final PipedOutputStream containerStdout = new PipedOutputStream();
+        in =new PipedInputStream(containerStdout);
+
+        final AttachContainerResultCallback callback = new AttachContainerResultCallback() {
+            public void onNext(Frame item) {
+                switch (item.getStreamType()) {
+                    case STDOUT:
+                    case RAW:
+                        try {
+                            containerStdout.write(item.getPayload());
+                        } catch (IOException e) {
+                            throw new DockerException("Failed to collect stdout", 0, e);
+                        }
+                }
+            }
+        };
 
         client.attachContainerCmd(container)
-                .withStdOut(true)
-                .withStdErr(true)
-                .withStdIn(containerStdin)
-                .withFollowStream(true)
-                .withLogs(true)
-                .exec(new AttachContainerResultCallback() {
-                    public void onNext(Frame item) {
-                        switch (item.getStreamType()) {
-                            case STDOUT:
-                            case RAW:
-                                try {
-                                    containerStdout.write(item.getPayload());
-                                } catch (IOException e) {
-                                    throw new DockerException("Failed to collect stdout", 0, e);
-                                }
-                        }
-                    }
-                });
-
-        slave.getComputer().setChannel(new PipedInputStream(containerStdout), new PipedOutputStream(containerStdin), TaskListener.NULL, new Channel.Listener() {
-            @Override
-            public void onClosed(Channel channel, IOException cause) {
-                System.out.println("Closed");
-            }
-        });
+              .withStdOut(true)
+              .withStdErr(true)
+              .withStdIn(containerStdin)
+              .withFollowStream(true)
+              .exec(callback);
     }
 
     private static class NoOpLauncher extends ComputerLauncher {
