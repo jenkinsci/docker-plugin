2d08a02ab5d4d974ca46dd456480f2d493d4a7f2
Nicolas De Loof <nicolas.deloof@gmail.com>
2017-09-11 09:54:45 +0200

    imports cleanup

--- docker-plugin/src/main/java/com/nirima/jenkins/plugins/docker/launcher/DockerComputerJNLPLauncher.java
+++ docker-plugin/src/main/java/com/nirima/jenkins/plugins/docker/launcher/DockerComputerJNLPLauncher.java
@@ -1,36 +1,13 @@
 package com.nirima.jenkins.plugins.docker.launcher;
 
-import com.github.dockerjava.api.DockerClient;
-import com.github.dockerjava.api.exception.DockerException;
-import com.github.dockerjava.api.command.CreateContainerCmd;
-import com.github.dockerjava.api.command.ExecCreateCmdResponse;
-import com.github.dockerjava.api.command.InspectContainerResponse;
-import com.github.dockerjava.api.exception.NotFoundException;
-import com.github.dockerjava.core.command.ExecStartResultCallback;
-import com.nirima.jenkins.plugins.docker.DockerComputer;
-import com.nirima.jenkins.plugins.docker.DockerTemplate;
-
 import hudson.Extension;
 import hudson.model.Descriptor;
-import hudson.model.TaskListener;
-import hudson.slaves.ComputerLauncher;
 import hudson.slaves.JNLPLauncher;
-import hudson.slaves.SlaveComputer;
-import hudson.util.TimeUnit2;
-import jenkins.model.Jenkins;
-import org.apache.commons.io.Charsets;
-import org.apache.commons.io.IOUtils;
-import org.kohsuke.accmod.Restricted;
-import org.kohsuke.accmod.restrictions.NoExternalUse;
 import org.kohsuke.stapler.DataBoundConstructor;
 import org.kohsuke.stapler.DataBoundSetter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import shaded.com.google.common.annotations.Beta;
-
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.PrintStream;
+import com.google.common.annotations.Beta;
 
 /**
  * JNLP launcher. Doesn't require open ports on docker host.
