2d0dda5a20401c34ad25dbf8df2b2948365b4f8e
Nicolas De Loof <nicolas.deloof@gmail.com>
2017-12-09 07:31:09 +0100

    use default timeout of 10 minutes to avoid JENKINS-47953

--- src/main/resources/com/nirima/jenkins/plugins/docker/strategy/DockerCloudRetentionStrategy/config.groovy
+++
@@ -1,7 +0,0 @@
-package com.nirima.jenkins.plugins.docker.strategy.DockerCloudRetentionStrategy
-
-def f = namespace(lib.FormTagLib);
-
-f.entry(title: "Idle timeout", field: "timeout") {
-    f.number(default: 0)
-}
--- src/main/resources/com/nirima/jenkins/plugins/docker/strategy/DockerOnceRetentionStrategy/config.groovy
+++ src/main/resources/com/nirima/jenkins/plugins/docker/strategy/DockerOnceRetentionStrategy/config.groovy
@@ -3,5 +3,5 @@ package com.nirima.jenkins.plugins.docker.strategy.DockerOnceRetentionStrategy
 def f = namespace(lib.FormTagLib);
 
 f.entry(title: "Idle timeout", field: "idleMinutes") {
-    f.number(default: 0)
+    f.number(default: 10)
 }
