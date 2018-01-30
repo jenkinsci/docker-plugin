55250747f24e3a160b28e2b979c60f3fb09c0b28
Patrick Ringl <patrick_@freenet.de>
2018-01-30 15:41:18 +0100

    Fix typo (DockerComputerAtatchConnectorTest => DockerComputerAttachConnectorTest

--- src/test/java/io/jenkins/docker/connector/DockerComputerAttachConnectorTest.java
+++ src/test/java/io/jenkins/docker/connector/DockerComputerAttachConnectorTest.java
@@ -8,7 +8,7 @@ import java.io.IOException;
 import java.net.URISyntaxException;
 import java.util.concurrent.ExecutionException;
 
-public class DockerComputerAtatchConnectorTest extends DockerComputerConnectorTest {
+public class DockerComputerAttachConnectorTest extends DockerComputerConnectorTest {
 
     @Test
     public void should_connect_agent() throws InterruptedException, ExecutionException, IOException, URISyntaxException {
