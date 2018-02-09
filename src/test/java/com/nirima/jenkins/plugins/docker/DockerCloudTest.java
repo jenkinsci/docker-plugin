d6c1d65696eeaf4cc8ba72b5811046ecbacd8e92
Peter Darton <pjdarton@gmail.com>
2018-02-09 10:56:31 +0000

    Unit test
    Added unit-test for the reworked code that counts the number of
    containers that are in the process of being spun up.

--- src/test/java/com/nirima/jenkins/plugins/docker/DockerCloudTest.java
+++ src/test/java/com/nirima/jenkins/plugins/docker/DockerCloudTest.java
@@ -71,4 +71,50 @@ public class DockerCloudTest {
 
         Assert.assertEquals(cloud, jenkins.getInstance().clouds.get(0));
     }
+
+    @Test
+    public void keepTrackOfContainersInProgress() {
+        final DockerTemplate i1 = new DockerTemplate(new DockerTemplateBase("image1"), null, null, null, null);
+        final DockerTemplate i2 = new DockerTemplate(new DockerTemplateBase("image2"), null, null, null, null);
+        final String uniqueId = Integer.toString(hashCode(), 36);
+        final DockerCloud c1 = new DockerCloud("cloud1." + uniqueId, null, null);
+        final DockerCloud c2 = new DockerCloud("cloud2." + uniqueId, null, null);
+
+        assertCount(c1, c2, i1, i2, 0, 0, 0, 0);
+        Assert.assertEquals("DockerCloud.CONTAINERS_IN_PROGRESS is empty to start with",
+                DockerCloud.CONTAINERS_IN_PROGRESS, Collections.EMPTY_MAP);
+
+        c1.incrementContainersInProgress(i1);
+        assertCount(c1, c2, i1, i2, 1, 0, 0, 0);
+        c1.decrementContainersInProgress(i2);
+        assertCount(c1, c2, i1, i2, 1, -1, 0, 0);
+        c2.incrementContainersInProgress(i1);
+        assertCount(c1, c2, i1, i2, 1, -1, 1, 0);
+        c2.incrementContainersInProgress(i1);
+        assertCount(c1, c2, i1, i2, 1, -1, 2, 0);
+
+        c1.decrementContainersInProgress(i1);
+        assertCount(c1, c2, i1, i2, 0, -1, 2, 0);
+        c1.incrementContainersInProgress(i2);
+        assertCount(c1, c2, i1, i2, 0, 0, 2, 0);
+        c2.decrementContainersInProgress(i1);
+        assertCount(c1, c2, i1, i2, 0, 0, 1, 0);
+        c2.decrementContainersInProgress(i1);
+        assertCount(c1, c2, i1, i2, 0, 0, 0, 0);
+        Assert.assertEquals("DockerCloud.CONTAINERS_IN_PROGRESS is empty afterwards",
+                DockerCloud.CONTAINERS_IN_PROGRESS, Collections.EMPTY_MAP);
+    }
+
+    private static void assertCount(DockerCloud c1, DockerCloud c2, DockerTemplate i1, DockerTemplate i2, int c1i1,
+            int c1i2, int c2i1, int c2i2) {
+        final int c1All = c1i1 + c1i2;
+        final int c2All = c2i1 + c2i2;
+        final String state = "when c1(" + c1i1 + "," + c1i2 + "), c2(" + c2i1 + "," + c2i2 + "), ";
+        Assert.assertEquals(state + "c1.countContainersInProgress()", c1All, c1.countContainersInProgress());
+        Assert.assertEquals(state + "c2.countContainersInProgress()", c2All, c2.countContainersInProgress());
+        Assert.assertEquals(state + "c1.countContainersInProgress(i1)", c1i1, c1.countContainersInProgress(i1));
+        Assert.assertEquals(state + "c1.countContainersInProgress(i2)", c1i2, c1.countContainersInProgress(i2));
+        Assert.assertEquals(state + "c2.countContainersInProgress(i1)", c2i1, c2.countContainersInProgress(i1));
+        Assert.assertEquals(state + "c2.countContainersInProgress(i2)", c2i2, c2.countContainersInProgress(i2));
+    }
 }
