ee8d9c10f89bef0d79a48c8fc9b3820531b585dc
Basil Crow <me@basilcrow.com>
2025-01-25 16:46:19 -0800

    Skip Docker-based tests when Docker is not available (#1125)

--- src/test/java/io/jenkins/docker/connector/DockerComputerAttachConnectorTest.java
+++ src/test/java/io/jenkins/docker/connector/DockerComputerAttachConnectorTest.java
@@ -6,13 +6,16 @@ import io.jenkins.docker.connector.DockerComputerAttachConnector.ArgumentVariabl
 import java.io.IOException;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeoutException;
+import org.junit.Assume;
 import org.junit.Test;
+import org.testcontainers.DockerClientFactory;
 
 public class DockerComputerAttachConnectorTest extends DockerComputerConnectorTest {
     private static final String ATTACH_AGENT_IMAGE_IMAGENAME = "jenkins/agent";
 
     @Test
     public void connectAgentViaDirectAttach() throws Exception {
+        Assume.assumeTrue(DockerClientFactory.instance().isDockerAvailable());
         final DockerComputerAttachConnector connector = new DockerComputerAttachConnector(COMMON_IMAGE_USERNAME);
         final String testName = "connectAgentViaDirectAttach";
         testAgentCanStartAndConnect(connector, testName);
@@ -20,6 +23,7 @@ public class DockerComputerAttachConnectorTest extends DockerComputerConnectorTe
 
     @Test
     public void connectAgentViaDirectAttachWithCustomCmd() throws Exception {
+        Assume.assumeTrue(DockerClientFactory.instance().isDockerAvailable());
         final DockerComputerAttachConnector connector = new DockerComputerAttachConnector(COMMON_IMAGE_USERNAME);
         // We could setJavaExe("/opt/jdk-11/bin/java") too, but that'd
         // mean we'd break the instant that the public docker image's JVM
--- src/test/java/io/jenkins/docker/connector/DockerComputerJNLPConnectorTest.java
+++ src/test/java/io/jenkins/docker/connector/DockerComputerJNLPConnectorTest.java
@@ -5,13 +5,16 @@ import com.nirima.jenkins.plugins.docker.DockerTemplateBase;
 import java.net.URI;
 import jenkins.model.JenkinsLocationConfiguration;
 import org.apache.commons.lang3.SystemUtils;
+import org.junit.Assume;
 import org.junit.Test;
+import org.testcontainers.DockerClientFactory;
 
 public class DockerComputerJNLPConnectorTest extends DockerComputerConnectorTest {
     private static final String JNLP_AGENT_IMAGE_IMAGENAME = "jenkins/inbound-agent";
 
     @Test
     public void connectAgentViaJNLP() throws Exception {
+        Assume.assumeTrue(DockerClientFactory.instance().isDockerAvailable());
 
         final JenkinsLocationConfiguration location = JenkinsLocationConfiguration.get();
         URI uri = URI.create(location.getUrl());
