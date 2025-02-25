8d3fd9aaae869b252fdb1629780ae6f5de2a9629
Jesse Glick <jglick@cloudbees.com>
2025-02-25 14:01:29 -0500

    Avoid excessive `Closure`s

--- src/main/resources/io/jenkins/docker/pipeline/DockerAgentScript.groovy
+++ src/main/resources/io/jenkins/docker/pipeline/DockerAgentScript.groovy
@@ -2,26 +2,24 @@ package io.jenkins.docker.pipeline
 
 import org.jenkinsci.plugins.pipeline.modeldefinition.Utils
 import org.jenkinsci.plugins.pipeline.modeldefinition.agent.CheckoutScript
-import org.jenkinsci.plugins.pipeline.modeldefinition.agent.DeclarativeAgentScript
+import org.jenkinsci.plugins.pipeline.modeldefinition.agent.DeclarativeAgentScript2
 import org.jenkinsci.plugins.workflow.cps.CpsScript
 
-class DockerAgentScript extends DeclarativeAgentScript<DockerAgent> {
+class DockerAgentScript extends DeclarativeAgentScript2<DockerAgent> {
 
     DockerAgentScript(CpsScript s, DockerAgent a) {
         super(s, a)
     }
 
     @Override
-    Closure run(Closure body) {
-        return {
-            try {
-                script.dockerNode(describable.asArgs) {
-                    CheckoutScript.doCheckout(script, describable, null, body).call()
-                }
-            } catch (Exception e) {
-                script.getProperty("currentBuild").result = Utils.getResultFromException(e)
-                throw e
+    void run(Closure body) {
+        try {
+            script.dockerNode(describable.asArgs) {
+                CheckoutScript.doCheckout2(script, describable, null, body)
             }
+        } catch (Exception e) {
+            script.getProperty("currentBuild").result = Utils.getResultFromException(e)
+            throw e
         }
     }
 
